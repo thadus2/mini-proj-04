1676d46a055b84c516657b52b8626109baced017
thadus2 <seunghoon3753@gmail.com>
2026-05-22 14:01:45 +0900

    base setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-
+    return(
+        <h1>프로젝트 시작</h1>
+    );
 }
 
 export default App
